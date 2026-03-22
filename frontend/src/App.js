8cecd8cd82fde7a1e155d9629b919b041bbc1a16
saksham780 <tiwarisaksham841@gmail.com>
2026-03-22 13:34:36 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'; 
+
+function App() { 
+  const [message, setMessage] = useState('Loading...'); 
+  
+  useEffect(() => { fetch('http://localhost:5000/') 
+    .then(res => res.json()) 
+    .then(data => setMessage(data.message)); 
+  }, []);
+
+  return (
+   <div>
+     <h1>{message}</h1>
+    </div>
+   ); 
+} 
+
+export default App;
