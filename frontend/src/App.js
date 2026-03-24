3bb3bdaa8a274438a2d361f3575735dd9ba36b59
saksham780 <tiwarisaksham841@gmail.com>
2026-03-24 19:20:49 +0530

    merge conflict resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
-import { useEffect, useState } from 'react'; 
-function App() { 
-  const [message, setMessage] = useState('Loading...'); 
-  useEffect(() => { fetch('http://localhost:5000/') 
-    .then(res => res.json()) 
-    .then(data => setMessage(data.message)); 
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('Loading...');
+
+  useEffect(() => {
+    fetch("https://frontend-zg29.onrender.com")
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
   }, []);
 
   return (
-   <div>
-     <h1>{message}</h1>
+    <div>
+      <h1>{message}</h1>
     </div>
-   ); 
-} 
+  );
+}
+
 export default App;
